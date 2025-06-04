0cd19a251c45d83f9119827a3cef91964318440a
Andreas <andreashev1872@gmail.com>
2025-06-04 23:00:06 +0700

    rename title

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const item = document.querySelector('.item');
 const placeholders = document.querySelectorAll('.placeholder');
+
 item.addEventListener('dragstart', dragstart);
 item.addEventListener('dragend', dragend);
+
 for (const placeholder of placeholders){
 placeholder.addEventListener('dragover', dragover);
 placeholder.addEventListener('dragenter', dragenter);
@@ -23,7 +25,7 @@ event.preventDefault();
 }
 
 function dragenter(event) {
-   event.target.classList.add('hovered');
+event.target.classList.add('hovered');
 }
 
 function dragleave(event) {
